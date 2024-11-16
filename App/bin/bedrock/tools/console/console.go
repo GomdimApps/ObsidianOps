67e95ca22eb977e8fae460040af3a876282b2304
GomdimApps <isackmota6565@gmail.com>
2024-11-15 23:19:20 -0300

    refactor(Console) replace lcme shell commands with exec.Command for session management

--- App/bin/bedrock/tools/console/console.go
+++ App/bin/bedrock/tools/console/console.go
@@ -3,10 +3,9 @@ package console
 import (
 	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/GomdimApps/ObsidianOps/App/bin/bedrock/tools/api/utils"
-
-	"github.com/GomdimApps/lcme"
 )
 
 var sessionName = "server-bedrock"
@@ -23,12 +22,12 @@ func StartSession(dir string) error {
 			return fmt.Errorf("diretório especificado '%s' não existe", dir)
 		}
 	}
-	if _, err := lcme.Shell("tmux has-session -t " + sessionName); err == nil {
+	if _, err := exec.Command("sh", "-c", "tmux has-session -t "+sessionName).Output(); err == nil {
 		fmt.Println("O server bedrock já foi iniciado!")
 		return nil
 	}
 	cmd := fmt.Sprintf("cd %s ; truncate -s 0 /var/log/bedrock-console.log ; LD_LIBRARY_PATH=. ./bedrock_server | tee -a /var/log/bedrock-console.log", dir)
-	if _, err := lcme.Shell("tmux new-session -d -s " + sessionName + " " + cmd); err != nil {
+	if _, err := exec.Command("sh", "-c", "tmux new-session -d -s "+sessionName+" "+cmd).Output(); err != nil {
 		return fmt.Errorf("erro ao iniciar a sessão tmux: %v", err)
 	}
 	fmt.Printf("Iniciando o server bedrock no diretório '%s'...\n", dir)
@@ -36,11 +35,11 @@ func StartSession(dir string) error {
 }
 
 func StopSession() error {
-	if _, err := lcme.Shell("tmux has-session -t " + sessionName); err == nil {
-		if _, err := lcme.Shell("tmux kill-session -t " + sessionName); err != nil {
+	if _, err := exec.Command("sh", "-c", "tmux has-session -t "+sessionName).Output(); err == nil {
+		if _, err := exec.Command("sh", "-c", "tmux kill-session -t "+sessionName).Output(); err != nil {
 			return fmt.Errorf("erro ao parar a sessão tmux: %v", err)
 		}
-		if _, err := lcme.Shell("truncate -s 0 /var/log/bedrock-console.log"); err != nil {
+		if _, err := exec.Command("sh", "-c", "truncate -s 0 /var/log/bedrock-console.log").Output(); err != nil {
 			return fmt.Errorf("erro ao truncar o arquivo de log: %v", err)
 		}
 		fmt.Println("Parando Server Bedrock")
@@ -51,8 +50,8 @@ func StopSession() error {
 }
 
 func ConnectSession() error {
-	if _, err := lcme.Shell("tmux has-session -t " + sessionName); err == nil {
-		if _, err := lcme.Shell("tmux attach -t " + sessionName); err != nil {
+	if _, err := exec.Command("sh", "-c", "tmux has-session -t "+sessionName).Output(); err == nil {
+		if _, err := exec.Command("sh", "-c", "tmux attach -t "+sessionName).Output(); err != nil {
 			return fmt.Errorf("erro ao conectar à sessão tmux: %v", err)
 		}
 	} else {
@@ -62,8 +61,8 @@ func ConnectSession() error {
 }
 
 func SendCommand(command string) error {
-	if _, err := lcme.Shell("tmux has-session -t " + sessionName); err == nil {
-		if _, err := lcme.Shell("tmux send-keys -t " + sessionName + " " + command + " C-m"); err != nil {
+	if _, err := exec.Command("sh", "-c", "tmux has-session -t "+sessionName).Output(); err == nil {
+		if _, err := exec.Command("sh", "-c", "tmux send-keys -t "+sessionName+" "+command+" C-m").Output(); err != nil {
 			return fmt.Errorf("erro ao enviar comando: %v", err)
 		}
 		fmt.Printf("Comando '%s' enviado para o server bedrock.\n", command)
